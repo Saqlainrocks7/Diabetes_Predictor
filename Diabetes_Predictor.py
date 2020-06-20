dddd55c0653b58899c5d5dce8e083013af87bce2
Saqlainrocks7 <48888895+Saqlainrocks7@users.noreply.github.com>
2020-06-20 18:26:59 +0530

    Add files via upload

---
+++ Diabetes_Predictor.py
@@ -0,0 +1,33 @@
+
+import numpy as np
+import pandas as pd
+import pickle
+
+df = pd.read_csv('diabetes.csv')
+
+df = df.rename(columns={'DiabetesPedigreeFunction':'DPF'})
+
+
+df[['Glucose','BloodPressure','SkinThickness','Insulin','BMI']] = df[['Glucose','BloodPressure','SkinThickness','Insulin','BMI']].replace(0,np.NaN)
+
+df['Glucose'].fillna(df['Glucose'].mean(), inplace=True)
+df['BloodPressure'].fillna(df['BloodPressure'].mean(), inplace=True)
+df['SkinThickness'].fillna(df['SkinThickness'].median(), inplace=True)
+df['Insulin'].fillna(df['Insulin'].median(), inplace=True)
+df['BMI'].fillna(df['BMI'].median(), inplace=True)
+
+# Model Building
+from sklearn.model_selection import train_test_split
+X = df.drop(columns='Outcome')
+y = df['Outcome']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=0)
+
+# Creating Random Forest Model
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators=20)
+classifier.fit(X_train, y_train)
+
+# Creating a pickle file for the classifier
+filename = 'diabetes_prediction_model.pkl'
+pickle.dump(classifier, open(filename, 'wb'))
+
